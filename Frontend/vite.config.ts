aa761cfd1eb70e0abff7941310b744b80c8d4f5d
Shweta-kush <kushishweta@gmail.com>
2025-09-29 01:38:50 +0530

    changes viteconfig

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: "/GuideLight/",
   base: "/GuideLight/",
+
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
